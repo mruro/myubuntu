c23f6f98bdd15ac63b31dbd06d003f3431939d9b
Jordi Íñigo <jig@safelayer.com>
2016-04-19 16:05:52 +0200

    Interim

--- install.sh
+++ install.sh
@@ -46,6 +46,7 @@ myubuntu() {
   # config Byobu
   byobu-ctrl-a emacs
 
+  if [ ! -d ~/.byobu ]; then
   cat > ~/.byobu/.tmux.conf <<EOF
 set -g mouse-select-pane on
 set -g mouse-select-window on
@@ -61,21 +62,26 @@ EOF
   
   # Powerline
   mkdir -p ~/git/src/github.com/powerline
-  cd ~/git/src/github.com/powerline
-  git clone https://github.com/powerline/fonts.git
-  cd fonts
-  ./install.sh
+  if [ ! -d fonts ]; then
+      cd ~/git/src/github.com/powerline
+      git clone https://github.com/powerline/fonts.git
+      cd fonts
+      ./install.sh
+  fi
+
   # botó dret, i selecciona un font adaptat a Powerline
   # per exemple: Liberation Mono for Powerline
 
   ###################################
   # Docker
-  sudo apt-get install apt-transport-https ca-certificates
+  sudo apt-get -y install apt-transport-https ca-certificates
   sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
   
-  sudo su -
-  echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
-  exit
+  if [ ! -e /etc/apt/sources.list.d/docker.list ]; then
+      echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /tmp/docker.list
+      sudo mv /tmp/docker.list /etc/apt/sources.list.d/docker.list
+  fi
+
   sudo apt-get update
   sudo apt-get -y purge lxc-docker
   sudo apt-get -y install linux-image-extra-$(uname -r)
@@ -94,8 +100,6 @@ EOF
   curl -L https://github.com/docker/machine/releases/download/v0.7.0/docker-machine-`uname -s`-`uname -m` > /tmp/docker-machine 
   sudo mv /tmp/docker-machine /usr/local/bin/docker-machine 
   sudo chmod +x /usr/local/bin/docker-machine
-
-  exit
 }
 
 # Check if reboot is needed
