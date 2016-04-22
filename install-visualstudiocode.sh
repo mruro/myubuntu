4b5fd5a63fb229608c594b199f70ce1adc373977
Jordi Íñigo <jig@safelayer.com>
2016-04-22 19:15:18 +0200

    Interim (add settings.json to Visual Studio Code)

--- install-visualstudiocode.sh
+++ install-visualstudiocode.sh
@@ -1,4 +1,4 @@
-minimal() {
+vsc() {
   # Use colors, but only if connected to a terminal, and that terminal
   # supports them.
   if which tput >/dev/null 2>&1; then
@@ -28,9 +28,30 @@ minimal() {
   # Visual Studio Code (Microsoft)
   printf "${BLUE}Installing Visual Studio Code (Microsoft)...${NORMAL}\n"
   sudo apt-get -y install git
+  # version 1.0.0:
   wget https://az764295.vo.msecnd.net/stable/fa6d0f03813dfb9df4589c30121e9fcffa8a8ec8/vscode-amd64.deb -O /tmp/vscode.deb
   sudo dpkg -i /tmp/vscode.deb
+
+    if [ ! -e ~/.vscode/settings.json ]; then
+      cat > ~/.vscode/settings.json <<EOF
+{
+    "go.buildOnSave": true,
+    "go.lintOnSave": true,
+    "go.vetOnSave": true,
+    "go.buildTags": "",
+    "go.buildFlags": [],
+    "go.lintFlags": [],
+    "go.vetFlags": [],
+    "go.coverOnSave": false,
+    "go.useCodeSnippetsOnFunctionSuggest": false,
+    "go.formatOnSave": true,
+    "go.formatTool": "goreturns",
+    "go.goroot": "~/go",
+    "go.gopath": "~/git",
+    "go.gocodeAutoBuild": false
+}
+EOF
 }
 
 # Check if reboot is needed
-minimal
+vsc
