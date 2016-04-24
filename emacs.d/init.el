fe3c7d79841b9dcf1a90c956d75b8f4189c42ab7
Jordi Íñigo <jig@safelayer.com>
2016-04-24 21:03:55 +0200

    Added minimal Go config for emacs

---
+++ emacs.d/init.el
@@ -0,0 +1,14 @@
+(require 'package)
+(add-to-list 'package-archives
+   '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(add-to-list 'load-path "~/.emacs.d/golang")
+(require 'go-mode-autoloads)
+
+(setq backup-directory-alist
+  `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+  `((".*" ,temporary-file-directory t)))
+
+# From http://tleyden.github.io/blog/2014/05/22/configure-emacs-as-a-go-editor-from-scratch/ below
+
