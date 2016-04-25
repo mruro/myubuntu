1b9f45a64e86bfd306951ad809c0a72c48fc8720
Jordi Íñigo <jig@users.noreply.github.com>
2016-04-25 16:37:31 +0200

    Removed verbosity on Go package installation

--- install-golang.sh
+++ install-golang.sh
@@ -68,15 +68,15 @@ EOF
 
   # Install debugging, testing, linting tools (Meet Visual Studio Code 1.0.0 requirements)
   printf "${BLUE}Installing some Go tools...${NORMAL}\n"
-  go get -u -v github.com/nsf/gocode
-  go get -u -v github.com/rogpeppe/godef
-  go get -u -v github.com/golang/lint/golint
-  go get -u -v github.com/lukehoban/go-outline
-  go get -u -v sourcegraph.com/sqs/goreturns
-  go get -u -v golang.org/x/tools/cmd/gorename
-  go get -u -v github.com/tpng/gopkgs
-  go get -u -v github.com/newhook/go-symbols
-  go get -u -v golang.org/x/tools/cmd/guru
+  go get -u github.com/nsf/gocode
+  go get -u github.com/rogpeppe/godef
+  go get -u github.com/golang/lint/golint
+  go get -u github.com/lukehoban/go-outline
+  go get -u sourcegraph.com/sqs/goreturns
+  go get -u golang.org/x/tools/cmd/gorename
+  go get -u github.com/tpng/gopkgs
+  go get -u github.com/newhook/go-symbols
+  go get -u golang.org/x/tools/cmd/guru
 }
 
 # Check if reboot is needed
