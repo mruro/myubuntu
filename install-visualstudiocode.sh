e4874faa343aace8fa871d321e8ea79fcd2e8511
Jose Manuel Rivera <jose.manuel.rivera@safelayer.com>
2016-05-02 16:48:16 +0200

    Support for Ubuntu Server in install-visualstudiocode.sh

--- install-visualstudiocode.sh
+++ install-visualstudiocode.sh
@@ -27,7 +27,7 @@ vsc() {
   ###################################
   # Visual Studio Code (Microsoft)
   printf "${BLUE}Installing Visual Studio Code (Microsoft)...${NORMAL}\n"
-  sudo apt-get -y install git
+  sudo apt-get -y install git libgtk2.0-0 libgconf-2-4 libasound2 libnss3 libxtst6
   # version 1.0.0:
   wget https://az764295.vo.msecnd.net/stable/fa6d0f03813dfb9df4589c30121e9fcffa8a8ec8/vscode-amd64.deb -O /tmp/vscode.deb
   sudo dpkg -i /tmp/vscode.deb
