731be23a1c6aba88f749253a674371c5c71ae5ce
Miguel Ruiz <miguel.ruiz@safelayer.com>
2016-05-03 15:31:48 +0200

    Upgrade not needed

--- install-distroupdate.sh
+++ install-distroupdate.sh
@@ -26,7 +26,6 @@ distroupgrade() {
  
   printf "${BLUE}Upgrading Ubuntu...${NORMAL}\n"
   sudo apt-get update
-  sudo apt-get -y upgrade
   sudo apt-get -y dist-upgrade
   sudo apt-get -y autoremove
 }
